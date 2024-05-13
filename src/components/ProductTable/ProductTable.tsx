33e95f6850c07ff13bd82c0dc95d93e116d808e6
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-05-13 19:01:27 +0200

    MIK-97 replaced produkty content with table

---
+++ src/components/ProductTable/ProductTable.tsx
@@ -0,0 +1,97 @@
+import { ProductModel2, ProductType2 } from "@/types";
+import { Rectangle } from "../Rectangle/Rectangle";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import styles from "./ProductTable.module.css";
+import classNames from "classnames";
+
+interface IProductTableProps {
+  className?: string;
+  items?: ProductModel2[];
+}
+
+const ProductTable: React.FC<IProductTableProps> = ({ className, items }) => {
+  return (
+    <Table className={classNames(className, styles["ProductTable"])}>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[50px]">Nr.</TableHead>
+          <TableHead className="w-[100px]">Nazwa</TableHead>
+          <TableHead className="w-[100px]">Typ</TableHead>
+          <TableHead className="w-[100px]">Rozmiar</TableHead>
+          <TableHead className="w-[100px]">Cena</TableHead>
+          <TableHead className="w-[100px]">Gotowe</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {items?.map((item, key) => (
+          <TableRow
+            className={classNames({ "bg-green-200": item.checked })}
+            key={key}
+          >
+            <TableCell>
+              <p>{key + 1}</p>
+            </TableCell>
+            <TableCell>
+              <p>{item.name}</p>
+            </TableCell>
+            <TableCell>
+              <p>{productTypeToType(item.type)}</p>
+            </TableCell>
+            <TableCell>
+              <p>{productTypeToSize(item.type)}</p>
+            </TableCell>
+            <TableCell>
+              <p>{item.price?.toFixed(2)} zł</p>
+            </TableCell>
+            <TableCell>
+              <Rectangle className="cursor-pointer" selected={item.checked} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+const productTypeToType = (type: ProductType2) => {
+  switch (type) {
+    case ProductType2.Drink:
+      return "Napój";
+    case ProductType2.PizzaBig:
+      return "Pizza";
+    case ProductType2.PizzaMedium:
+      return "Pizza";
+    case ProductType2.PizzaSmall:
+      return "Pizza";
+    case ProductType2.Sauce:
+      return "Sos";
+    case ProductType2.Snack:
+      return "Przekąska";
+    default:
+      throw new Error(`Type ${type} not defined`);
+  }
+};
+
+const productTypeToSize = (type: ProductType2) => {
+  switch (type) {
+    case ProductType2.PizzaBig:
+      return "Duża";
+    case ProductType2.PizzaMedium:
+      return "Średnia";
+    case ProductType2.PizzaSmall:
+      return "Mała";
+    default:
+      return "-";
+  }
+};
+
+export { ProductTable };
--- src/views/OrderView/OrderView.tsx
+++ src/views/OrderView/OrderView.tsx
@@ -1,14 +1,54 @@
 import styles from "./OrderView.module.css";
 import { OrderTable } from "@/components";
 import { useAppContext } from "@/context/AppContext";
+import { OrderModel, ProductType2 } from "@/types";
 
 const OrderView = () => {
-  const [app] = useAppContext();
+  const [app, updateApp] = useAppContext();
+
+  const handleRowClick = (item: OrderModel) => {
+    if (item.id === app!.selectedOrder?.id) {
+      return;
+    }
+
+    const newItem = {
+      ...item,
+      products: [
+        {
+          name: "Pizza",
+          id: "1",
+          price: 20,
+          checked: false,
+          type: ProductType2.Drink,
+        },
+        {
+          name: "Super ekstra pizza i cos tam jeszcze",
+          id: "2",
+          price: 22,
+          checked: true,
+          type: ProductType2.PizzaBig,
+        },
+        {
+          name: "Super",
+          id: "3",
+          price: 22,
+          checked: false,
+          type: ProductType2.PizzaBig,
+        },
+      ],
+    };
+
+    updateApp("selectedOrder", newItem);
+  };
 
   return (
     <div className={styles["OrderView"]}>
       <h1>Zamówienia</h1>
-      <OrderTable items={app?.orders} />
+      <OrderTable
+        items={app?.orders}
+        selectedItem={app?.selectedOrder}
+        onRowClick={handleRowClick}
+      />
     </div>
   );
 };
